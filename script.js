b351efa574a464e608460aa3e13603ce7eab0a92
YassinOmar <yassin.o.darwesh@gmail.com>
2023-09-23 11:12:56 +0300

    menu-fix

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navbarToggler = document.querySelector("[data-nav-toggler]")
 
 navbarToggler.addEventListener("click", function () {
+    console.log('clicked')
     navbar.classList.toggle("active");
     this.classList.toggle("active");
 });
